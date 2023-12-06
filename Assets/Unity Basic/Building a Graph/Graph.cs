1956cb197486436ba842c820ff228959f14db82e
Stickguy1 <adamkaci3@csu.fullerton.edu>
2023-12-05 23:02:24 -0800

    Building a graph done

---
+++ Assets/Unity Basic/Building a Graph/Graph.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+    Transform[] points;
+
+    [SerializeField, Range(10, 10000)]
+    int resolution = 10;
+
+    // X coordinates handled here because they won't change
+    void Awake()
+    {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    // Y coordinates handled here because they'll be updating constantly
+    void Update ()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length;i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
