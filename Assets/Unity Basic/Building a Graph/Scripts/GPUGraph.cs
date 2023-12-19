b2915248c9d4cf82fb0269bb56c25570d7fb6119
Adam101k <111901655+Adam101k@users.noreply.github.com>
2023-12-19 15:12:54 -0800

    Starting Procedural Drawing

--- Assets/Unity Basic/Building a Graph/Scripts/GPUGraph.cs
+++ Assets/Unity Basic/Building a Graph/Scripts/GPUGraph.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class GPUGraph : MonoBehaviour
 {
+    [SerializeField]
+    ComputeShader computeShader;
+
     [SerializeField, Range(10, 200)]
     int resolution = 10;
 
@@ -25,6 +28,12 @@ public class GPUGraph : MonoBehaviour
     Functionlibrary.FunctionName transitionFunction;
 
     ComputeBuffer positionsBuffer;
+
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_Time");
 
     void OnEnable()
     {
@@ -56,6 +65,19 @@ public class GPUGraph : MonoBehaviour
             PickNextFunction();
         }
     }
+
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+
+    }
+
     void PickNextFunction()
     {
         function = transitionMode == TransitionMode.Cycle ?
